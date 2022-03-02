4a042fa37999006e4d4165ce524ef9bd4fb41128
Samuel Chapleau <sachaple@microsoft.com>
2022-03-02 12:17:42 -0800

    Add project files.

---
+++ NavigationViewCrashes/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+
+namespace NavigationViewCrashes
+{
+    public class Separator { }
+
+    public class Menu
+    {
+        public string Name { get; set; }
+        public IconSource Icon { get; set; }
+
+        public ObservableCollection<Menu> SubMenus { get; set; }
+
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        private const int MaxNestingDepth = 5;
+        private const int SubMenusCount = 5;
+
+        ObservableCollection<Menu> Menus { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BuildMenus();
+        }
+
+        private void BuildMenus()
+        {
+            Menus = new ObservableCollection<Menu>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Menus.Add(new Menu { Name = $"Menu {i}", Icon = new FontIconSource { Glyph = "\uE783" } });
+            }
+
+            BuildNestedMenus(Menus, 0);
+        }
+
+        private void BuildNestedMenus(ObservableCollection<Menu> menus, int currentNestingDepth)
+        {
+            if (currentNestingDepth == MaxNestingDepth)
+            {
+                return;
+            }
+
+            foreach (var menu in menus)
+            {
+                menu.SubMenus = new ObservableCollection<Menu>();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    menu.SubMenus.Add(new Menu { Name = $"Menu {i}", Icon = new FontIconSource { Glyph = "\uE783" } });
+                }
+
+                BuildNestedMenus(menu.SubMenus, currentNestingDepth + 1);
+            }
+        }
+    }
+}
---
+++ NavigationViewCrashes/Selectors/MenuItemTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace NavigationViewCrashes.Selectors
+{
+    [ContentProperty(Name = "ItemTemplate")]
+    public class MenuItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ItemTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            return item is Separator ? SeparatorTemplate : ItemTemplate;
+        }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return item is Separator ? SeparatorTemplate : ItemTemplate;
+        }
+
+        internal DataTemplate SeparatorTemplate = (DataTemplate)XamlReader.Load(
+    @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <NavigationViewItemSeparator />
+                  </DataTemplate>");
+
+    }
+}
